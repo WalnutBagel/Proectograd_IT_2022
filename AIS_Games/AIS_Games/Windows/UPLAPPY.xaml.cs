825421b7156138eb12c73fca3e585d3b17a468dd
UPLAPPY <107671246+UPLAPPY@users.noreply.github.com>
2022-06-23 12:23:53 +0300

    работаю с финальным окном

--- AIS_Games/AIS_Games/Windows/UPLAPPY.xaml.cs
+++ AIS_Games/AIS_Games/Windows/UPLAPPY.xaml.cs
@@ -45,96 +45,112 @@ namespace AIS_Games.Windows
             {
                 string final = "krestik";
                 Windows.final win_k = new Windows.final();
+                Close();
                 win_k.ShowDialog();
             }
             else if (bt1 == "n" && bt4 == "n" && bt7 == "n")
             {
                 string final = "nolik";
                 Windows.final win_n = new Windows.final();
+                Close();
                 win_n.ShowDialog();
             }
             else if (bt2 == "k" && bt5 == "k" && bt8 == "k")
             {
                 string final = "krestik";
                 Windows.final win_k = new Windows.final();
+                Close();
                 win_k.ShowDialog();
             }
             else if (bt2 == "n" && bt5 == "n" && bt8 == "n")
             {
                 string final = "nolik";
                 Windows.final win_n = new Windows.final();
+                Close();
                 win_n.ShowDialog();
             }
             else if (bt3 == "k" && bt6 == "k" && bt9 == "k")
             {
                 string final = "krestik";
                 Windows.final win_k = new Windows.final();
+                Close();
                 win_k.ShowDialog();
             }
             else if (bt3 == "n" && bt6 == "n" && bt9 == "n")
             {
                 string final = "nolik";
                 Windows.final win_n = new Windows.final();
+                Close();
                 win_n.ShowDialog();
             }
             else if (bt1 == "k" && bt2 == "k" && bt3 == "k")
             {
                 string final = "krestik";
                 Windows.final win_k = new Windows.final();
+                Close();
                 win_k.ShowDialog();
             }
             else if (bt1 == "n" && bt2 == "n" && bt3 == "n")
             {
                 string final = "nolik";
                 Windows.final win_n = new Windows.final();
+                Close();
                 win_n.ShowDialog();
             }
             else if (bt4 == "k" && bt5 == "k" && bt6 == "k")
             {
                 string final = "krestik";
                 Windows.final win_k = new Windows.final();
+                Close();
                 win_k.ShowDialog();
             }
             else if (bt4 == "n" && bt5 == "n" && bt6 == "n")
             {
                 string final = "nolik";
                 Windows.final win_n = new Windows.final();
+                Close();
                 win_n.ShowDialog();
             }
             else if (bt7 == "k" && bt8 == "k" && bt9 == "k")
             {
                 string final = "krestik";
                 Windows.final win_k = new Windows.final();
+                Close();
                 win_k.ShowDialog();
             }
             else if (bt7 == "n" && bt8 == "n" && bt9 == "n")
             {
                 string final = "nolik";
                 Windows.final win_n = new Windows.final();
+                Close();
                 win_n.ShowDialog();
             }
             else if (bt1 == "k" && bt5 == "k" && bt9 == "k")
             {
                 string final = "krestik";
                 Windows.final win_k = new Windows.final();
+                Close();
                 win_k.ShowDialog();
             }
             else if (bt1 == "n" && bt5 == "n" && bt9 == "n")
             {
                 string final = "nolik";
                 Windows.final win_k = new Windows.final();
+                Close();
                 win_k.ShowDialog();
             }
             else if (bt3 == "k" && bt5 == "k" && bt7 == "k")
             {
                 string final = "krestik";
                 Windows.final win_k = new Windows.final();
+                Close();
                 win_k.ShowDialog();
             }
             else if (bt3 == "n" && bt5 == "n" && bt7 == "n")
             {
                 string final = "nolik";
                 Windows.final win_n = new Windows.final();
+                Close();
                 win_n.ShowDialog();
             }
         }
@@ -147,12 +163,14 @@ namespace AIS_Games.Windows
                 {
                     bt1 = "k";
                     hod = "nolik";
-                    but1.ImageSource = new BitmapImage(new Uri("/Resources/UPLAPPY/крестик.png", UriKind.RelativeOrAbsolute));
+                    //but1.ImageSource = new BitmapImage(new Uri("/Resources/UPLAPPY/крестик.png", UriKind.RelativeOrAbsolute));
+                    proverka();
                 }
                 else if (hod == "nolik")
                 {
                     bt1 = "n";
                     hod = "krestik";
+                    proverka();
                 }
             }
         }
@@ -164,11 +182,13 @@ namespace AIS_Games.Windows
                 {
                     bt2 = "k";
                     hod = "nolik";
+                    proverka();
                 }
                 else if (hod == "nolik")
                 {
                     bt2 = "n";
                     hod = "krestik";
+                    proverka();
                 }
             }
         }
@@ -181,11 +201,13 @@ namespace AIS_Games.Windows
                 {
                     bt3 = "k";
                     hod = "nolik";
+                    proverka();
                 }
                 else if (hod == "nolik")
                 {
                     bt3 = "n";
                     hod = "krestik";
+                    proverka();
                 }
             }
         }
@@ -197,11 +219,13 @@ namespace AIS_Games.Windows
                 {
                     bt4 = "k";
                     hod = "nolik";
+                    proverka();
                 }
                 else if (hod == "nolik")
                 {
                     bt4 = "n";
                     hod = "krestik";
+                    proverka();
                 }
             }
         }
@@ -213,11 +237,13 @@ namespace AIS_Games.Windows
                 {
                     bt5 = "k";
                     hod = "nolik";
+                    proverka();
                 }
                 else if (hod == "nolik")
                 {
                     bt5 = "n";
                     hod = "krestik";
+                    proverka();
                 }
             }
         }
@@ -229,11 +255,13 @@ namespace AIS_Games.Windows
                 {
                     bt6 = "k";
                     hod = "nolik";
+                    proverka();
                 }
                 else if (hod == "nolik")
                 {
                     bt6 = "n";
                     hod = "krestik";
+                    proverka();
                 }
             }
         }
@@ -245,11 +273,13 @@ namespace AIS_Games.Windows
                 {
                     bt7 = "k";
                     hod = "nolik";
+                    proverka();
                 }
                 else if (hod == "nolik")
                 {
                     bt7 = "n";
                     hod = "krestik";
+                    proverka();
                 }
             }
         }
@@ -261,11 +291,13 @@ namespace AIS_Games.Windows
                 {
                     bt8 = "k";
                     hod = "nolik";
+                    proverka();
                 }
                 else if (hod == "nolik")
                 {
                     bt8 = "n";
                     hod = "krestik";
+                    proverka();
                 }
             }
         }
@@ -277,11 +309,13 @@ namespace AIS_Games.Windows
                 {
                     bt9 = "k";
                     hod = "nolik";
+                    proverka();
                 }
                 else if (hod == "nolik")
                 {
                     bt9 = "n";
                     hod = "krestik";
+                    proverka();
                 }
             }
         }
